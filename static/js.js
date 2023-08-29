8f742ee5108658f228c2b9fb33cdad58747fca3f
klaramari <klara.fleisch@gmail.com>
2023-08-29 09:46:32 +0200

    Add functionality for random color and color darkening
    User can now choose random or dark

--- static/js.js
+++ static/js.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", makeRectangles(16, "blue"));
+document.addEventListener("DOMContentLoaded", makeRectangles(16, "darker"));
 
 function makeRectangles(side, color){
     let cont;
@@ -19,21 +19,41 @@ function makeRectangles(side, color){
             //div.innerHTML = ''
             div.className = "thedivs"
             sides = 960/side;
-
+            if (color == "darker"){
+                div.style.backgroundColor = "antiquewhite";
+            }
+            div.style.filter = "brightness(1)";
             div.style.width = sides + "px";
             div.style.height = sides + "px";
             cont.appendChild(div);
         }
     }
     const divs = document.querySelectorAll(".thedivs");
-
+    let colornow;
     divs.forEach((div) => {
         div.addEventListener("mouseover", function (e){
+            
+            if (color == "random"){
+                e.target.style.background = "#" + Math.floor(Math.random()*16777215).toString(16);
+            }
+            else if (color == "darker"){
+                colornow = e.target.style.filter;
+                console.log(colornow);
+                
+                colornow = (Number(colornow.slice(11,-1)) * 0.9).toFixed(2);
+                
+                console.log(colornow);
+                e.target.style.filter = "brightness(" + colornow + ")"; 
+            }
+            else {
             e.target.style.background = color;
+            }
         })
     })
     
 }
+
+
 
 const opener = document.querySelector(".thebutton");
 opener.addEventListener("click", changeRectangles);
@@ -43,8 +63,13 @@ function changeRectangles (){
     do {
     side = prompt("How many squares per side? (max 100)");
     } while (side > 100 || !Number(side));
-    makeRectangles(side, "blue");
+    let color;
+   
+    color = prompt("Which color to paint?");
+    makeRectangles(side, color);
 };
+
+
 
 // const closer = document.querySelector("#nein");
 // closer.addEventListener("click", function (){closePopup()});
